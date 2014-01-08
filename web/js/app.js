4b5848da3955549bcc9571858f59af9b7bb3aea1
Max Martinez <max@max.uy>
2014-01-08 00:02:28 -0200

    web ui with last 6 hours chart

--- web/js/app.js
+++ web/js/app.js
@@ -1,7 +1,12 @@
 (function() {
 
-  var pollPulseDelta = 2*60*60*1000; // 2 hours
+  var pollPulsesHours = 6
+  var pollPulseMillis = pollPulsesHours*60*60*1000;
   var lastPulses = []
+  var chartsReady = false
+
+  google.load('visualization', '1.0', {'packages':['corechart']});
+  google.setOnLoadCallback(function() { chartsReady = true; });
 
   $(document).ready(function() {
     pollPulses();
@@ -9,17 +14,42 @@
 
   function pollPulses() {
     var time = new Date().getTime();
-    $.get("range", { from: time - pollPulseDelta },function(data) {
+    $.get("range", { from: time - pollPulseMillis },function(data) {
       lastPulses = data.Pulses;
       if (lastPulses.length > 2) {
-        var delta1 = new Date().getTime() - lastPulses[lastPulses.length-1]
-        var delta2 = lastPulses[lastPulses.length-1] - lastPulses[lastPulses.length-2];
-        var delta = delta1 > 60*60*10 ? delta1 : delta;
-        var watts = Math.round(60*60*1000/delta);
-        $('#currentWatts').text(watts.toString())
+        var watts = deltaToWatts(lastPulses[lastPulses.length-1] - lastPulses[lastPulses.length-2])
+        $('#wattsNow').text(watts.toString())
+        drawNowChart();
       }
       setTimeout(pollPulses, 2000);
     }, "json");
+  }
+
+  function drawNowChart() {
+    if(!chartsReady) return;
+    var watts = [['Time', 'Power']];
+    var lastT = 0;
+    $.each(lastPulses, function(i,t) {
+      if (i > 0) {
+        watts.push([new Date(t), deltaToWatts(t-lastT)])
+      }
+      lastT = t;
+    })
+    var data = google.visualization.arrayToDataTable(watts);
+
+    var options = {
+      title: 'Last ' + pollPulsesHours + ' hours',
+      hAxis: {
+        format: 'HH:mm'
+      }
+    };
+
+    var chart = new google.visualization.LineChart(document.getElementById('nowChart'));
+    chart.draw(data, options);
+  }
+
+  function deltaToWatts(delta) {
+    return Math.round(60*60*1000/delta);
   }
 
 })();
